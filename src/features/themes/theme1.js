cf73daf5515865ae701c881a2648346679ec8b59
Udayan Basak <thisisudayan@gmail.com>
2024-07-11 01:59:41 +0600

    sd

--- src/features/themes/theme1.js
+++ src/features/themes/theme1.js
@@ -4,12 +4,24 @@ import URLImage from "./urlImage";
 
 
 export default function Theme1() {
+    const data = [{
+        version:'Primary(English)',
+        class:'XI',
+        roll:'23',
+        year:'2024',
+        mobile:'1234567890',
+        pic: "https://firebasestorage.googleapis.com/v0/b/agamilabs-edubd.appspot.com/o/files%2Fstudent%2FEsGaQ_1719903359259.jpg?alt=media&token=8fe4166a-84ac-47ec-a777-de31121d130c",
+        name: "maruf bin solaiman siddique",
+        id:"4567887654"
+    },
+    ]
+
     return(
         <div> {/* page er size onujayi etar size hobe. dynamecilaay + felx */}
 
             {
                 /* map chalai then kaj korte hbe ekhane */
-                
+
                 <Stage width={204} height={324}>
                     <Layer>
                         <URLImage src="https://i.ibb.co/VH2y237/imagecard.png" x={0} y={0} scale={0.5} />
