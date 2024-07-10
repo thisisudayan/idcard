b2cc141f47d447c8098a804e43dd2e1038f67a2a
Udayan Basak <thisisudayan@gmail.com>
2024-07-11 01:57:13 +0600

    af

--- src/features/themes/theme1.js
+++ src/features/themes/theme1.js
@@ -5,22 +5,29 @@ import URLImage from "./urlImage";
 
 export default function Theme1() {
     return(
-        <Stage width={204} height={324}>
-            <Layer>
-                <URLImage src="https://i.ibb.co/VH2y237/imagecard.png" x={0} y={0} scale={0.5} />
-            </Layer>
-            <Layer>
-                <Text text="Udayan Basak" y={175} x={204/2} fontSize={16} fontStyle="bold"  fill={"black"} align="center" />
-            </Layer>
-            <Layer>
-                <Text text="Full name" />
-            </Layer>
-            <Layer>
-                <Text text="Full name" />
-            </Layer>
-            <Layer>
-                <Text text="Full name" />
-            </Layer>
-        </Stage>
+        <div> {/* page er size onujayi etar size hobe. dynamecilaay + felx */}
+
+            {
+                /* map chalai then kaj korte hbe ekhane */
+                
+                <Stage width={204} height={324}>
+                    <Layer>
+                        <URLImage src="https://i.ibb.co/VH2y237/imagecard.png" x={0} y={0} scale={0.5} />
+                    </Layer>
+                    <Layer>
+                        <Text text="Udayan Basak" y={175} x={204/2} fontSize={16} fontStyle="bold"  fill={"black"} align="center" />
+                    </Layer>
+                    <Layer>
+                        <Text text="Full name" />
+                    </Layer>
+                    <Layer>
+                        <Text text="Full name" />
+                    </Layer>
+                    <Layer>
+                        <Text text="Full name" />
+                    </Layer>
+                </Stage>
+            }
+        </div>
     )
 }
