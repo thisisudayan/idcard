5b00084b3cc865da7cbe2f3d98a37d2fa285c4be
Maruf Bin Solaiman <0909maruf@gmail.com>
2024-07-12 02:48:37 +0600

    detailed space fixed

--- src/features/themes/theme.js
+++ src/features/themes/theme.js
@@ -3,7 +3,7 @@ import URLImage from "./urlImage";
 import "./print.css"
 import { useSelector } from "react-redux"
 
-export default function Theme1() {
+export default function Theme() {
 
     const data = useSelector((state) => state.dashboard.idCards)
 
@@ -18,8 +18,8 @@ export default function Theme1() {
                         <Layer>
                             <URLImage src={item.photoname} x={52} y={62} width={100} height={100} radius={60} strokeColor="#3B75C4" strokeWidth={3} />
                             <Text text={item.name_en} fontSize={16} fontStyle="bold" y={175} width={185} align="center" />
-                            <Text y={210} x={20} align="left" fontFamily="Roboto" text={`ID NO\nVERSION\nCLASS\nROLL\nYEAR\nMOBILE`} />
-                            <Text y={210} x={100} align="left" fontFamily="Roboto" text={`: ${item.stdid}\n: ${item.courseleveltitle}\n: ${item.grouptitle}\n: ${item.rollno}\n: ${item.academicyear}\n: ${item.contactno}`} />
+                            <Text y={210} x={10} align="left" fontFamily="Roboto" text={`ID NO\nVERSION\nCLASS\nROLL\nYEAR\nMOBILE`} />
+                            <Text y={210} x={70} align="left" fontFamily="Roboto" text={`: ${item.stdid}\n: ${item.courseleveltitle}\n: ${item.grouptitle}\n: ${item.rollno}\n: ${item.academicyear}\n: ${item.contactno}`} />
                             <URLImage src={`https://barcodeapi.org/api/${item.stdid}?text=none`} x={22} y={293} width={130} height={20} radius={2} strokeColor="#ffffff" strokeWidth={3} />
                         </Layer>
                     </Stage>
