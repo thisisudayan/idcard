5e043cb65d923e07aff9cc1d0e8dde308aaf9dea
Maruf Bin Solaiman <0909maruf@gmail.com>
2024-07-17 03:11:50 +0600

    text overflow fixed.

--- src/features/themes/konva1.js
+++ src/features/themes/konva1.js
@@ -57,7 +57,7 @@ const generateThemeTwo = async (d) => {
 
         avatarImgObj.onload = () => {
             const darthNode = new Konva.Image({
-                y: 67,
+                y: 50,
                 x: (stage.width() - 100) / 2,
                 height: 100,
                 width: 100,
@@ -82,16 +82,16 @@ const generateThemeTwo = async (d) => {
         //////////////////////////////////
         var nameText = new Konva.Text({
             x: stage.width() / 2,
-            y: 175,
+            y: 158,
             text: `${d.name_en.toLocaleUpperCase()}`,
             text: `${d.name_en}`,
             fontSize: 16,
             fontStyle: 700,
             fontStyle: 'bold',
-            width: stage.width() - 20,
-            fontFamily: 'Calibri',
+            width: stage.width(),
+            fontFamily: 'Roboto Condensed',
             fill: 'black',
-            align: 'center'
+            align: 'center',
         })
         nameText.offsetX(nameText.width() / 2);
         nameLayer.add(nameText)
@@ -101,19 +101,42 @@ const generateThemeTwo = async (d) => {
 
     const promise4 = new Promise((resolve, reject) => {
         /////////////////////////////////////////////
-        //////  add additional text  ////////////////
+        //////  add additional text field name  ////////////////
         /////////////////////////////////////////////
         var additionalText = new Konva.Text({
-            x: stage.width() / 2,
-            y: 210,
-            text: `ID NO          : ${d.stdid}\nVERSION     : ${d.courseleveltitle.toLocaleUpperCase()}\nCLASS          : ${d.grouptitle}\nROLL            : ${d.rollno}\nYEAR            : ${d.academicyear}\nMOBILE       : ${d.contactno}`,
-            text: `ID NO          : ${d.stdid}\nVERSION     : ${d.courseleveltitle}\nCLASS          : ${d.grouptitle}\nROLL            : ${d.rollno}\nYEAR            : ${d.academicyear}\nMOBILE       : ${d.contactno}`,
+            x: (stage.width() / 2) + 10,
+            y: 190,
+            text: `ID NO\nVERSION\nCLASS\nROLL\nYEAR\nMOBILE`,
+            // text: `ID NO          : ${d.stdid}\nVERSION     : ${d.courseleveltitle.toLocaleUpperCase()}\nCLASS          : ${d.grouptitle}\nROLL            : ${d.rollno}\nYEAR            : ${d.academicyear}\nMOBILE       : ${d.contactno}`,
             fontSize: 12,
             // fontStyle:700,
-            width: stage.width() - 40,
-            fontFamily: 'Calibri',
+            width: stage.width() - 0,
+            fontFamily: 'Roboto Condensed',
             fill: 'black',
-            align: 'left'
+            align: 'left',
+            lineHeight:1.3
+        })
+
+        additionalText.offsetX(additionalText.width() / 2);
+        additionalLayer.add(additionalText)
+        resolve()
+    })
+    const promise5 = new Promise((resolve, reject) => {
+        /////////////////////////////////////////////
+        //////  add additional text field value  ////////////////
+        /////////////////////////////////////////////
+        var additionalText = new Konva.Text({
+            x: (stage.width() / 2) + 60,
+            y: 190,
+            text: `: ${d.stdid}\n: ${d.courseleveltitle.toLocaleUpperCase()}\n: ${d.grouptitle}\n: ${d.rollno}\n: ${d.academicyear}\n: ${d.contactno}`,
+            // text: `ID NO          : ${d.stdid}\nVERSION     : ${d.courseleveltitle.toLocaleUpperCase()}\nCLASS          : ${d.grouptitle}\nROLL            : ${d.rollno}\nYEAR            : ${d.academicyear}\nMOBILE       : ${d.contactno}`,
+            fontSize: 12,
+            // fontStyle:700,
+            width: stage.width() - 0,
+            fontFamily: 'Roboto Condensed',
+            fill: 'black',
+            align: 'left',
+            lineHeight:1.3
         })
 
         additionalText.offsetX(additionalText.width() / 2);
@@ -122,7 +145,7 @@ const generateThemeTwo = async (d) => {
     })
 
 
-    const promise5 = new Promise((resolve, reject) => {
+    const promise6 = new Promise((resolve, reject) => {
         ////////////////////////////////////
         ///////// add barcode image /////////
         ////////////////////////////////////
@@ -157,12 +180,15 @@ const generateThemeTwo = async (d) => {
 
 
 
-    return Promise.all([promise1, promise2, promise3, promise4, promise5]).then(async () => {
-        return stage.toBlob({ pixelRatio: 3 })
-        // .then(blob => resolve(blob))
-        // .catch(err => reject(err));
-    })
+    return Promise.all([promise1, promise2, promise3, promise4, promise5, promise6])
+        .then(async () => {
+            return stage.toBlob({ pixelRatio: 3 })
+            // .then(blob => resolve(blob))
+            // .catch(err => reject(err));
+        })
 
 }
 
 export default generateThemeTwo;
+
+
